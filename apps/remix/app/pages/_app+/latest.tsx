b01d19bcc92e93f1420b0cf8412090f44611efda
Jack Clackett <jack@noquarter.co>
2023-05-19 14:51:05 +0200

    fix latest spots

--- apps/remix/app/pages/_app+/latest.tsx
+++ apps/remix/app/pages/_app+/latest.tsx
@@ -7,21 +7,25 @@ import { createImageUrl } from "@ramble/shared"
 import { Button } from "@ramble/ui"
 
 import { db } from "~/lib/db.server"
+import { Camera } from "lucide-react"
 
 export const loader = async ({ request }: LoaderArgs) => {
   const searchParams = new URL(request.url).searchParams
-  const skip = searchParams.get("skip") || 0
+  const skip = parseInt((searchParams.get("skip") as string) || "0")
   const spots = await db.spot.findMany({
-    take: 2,
-    skip: parseInt(skip as string),
-    select: { id: true, name: true, images: { take: 1, orderBy: { createdAt: "asc" } } },
+    take: 10,
+    skip,
+    where: { images: { some: {} } },
+    // where: { verifiedAt: { not: { equals: null } }, images: { some: {} } },
+    select: { id: true, name: true, address: true, images: { select: { path: true }, take: 1, orderBy: { createdAt: "asc" } } },
     orderBy: { createdAt: "desc" },
   })
-  return json(spots)
+  const count = await db.spot.count({ where: { images: { some: {} } } })
+  return json({ spots, count })
 }
 
 export default function Latest() {
-  const initialSpots = useLoaderData<typeof loader>()
+  const { spots: initialSpots, count } = useLoaderData<typeof loader>()
 
   const spotFetcher = useFetcher<typeof loader>()
   const [spots, setSpots] = React.useState(initialSpots)
@@ -30,7 +34,7 @@ export default function Latest() {
 
   React.useEffect(() => {
     if (spotFetcher.state === "loading") return
-    const data = spotFetcher.data
+    const data = spotFetcher.data?.spots
     if (data) setSpots((prev) => [...prev, ...data])
   }, [spotFetcher.data, spotFetcher.state])
 
@@ -38,25 +42,35 @@ export default function Latest() {
     <div className="space-y-4 p-4 md:p-8">
       <h1 className="text-3xl">Latest Spots</h1>
       <div className="space-y-2">
-        {spots.map((spot, i) => (
-          <div key={spot.id} className="flex items-center space-x-2">
-            <img
-              alt="spot"
-              width={200}
-              height={100}
-              className="h-[100px] rounded object-cover"
-              src={spot.images[0]?.path ? createImageUrl(spot.images[0].path) : undefined}
-            />
-            <Link to={`/spots/${spot.id}`}>
-              <p>{spot.name}</p>
-            </Link>
-          </div>
+        {spots.map((spot) => (
+          <Link to={`/spots/${spot.id}`} key={spot.id} className="flex items-center space-x-2 hover:opacity-70">
+            {spot.images[0] ? (
+              <img
+                alt="spot"
+                width={200}
+                height={100}
+                className="h-[100px] min-w-[200px] rounded bg-gray-50 object-cover dark:bg-gray-700"
+                src={spot.images[0].path ? createImageUrl(spot.images[0].path) : undefined}
+              />
+            ) : (
+              <div className="flex h-[100px] min-w-[200px] items-center justify-center rounded bg-gray-50 object-cover dark:bg-gray-700">
+                <Camera className="opacity-50" />
+              </div>
+            )}
+
+            <div>
+              <p className="text-2xl">{spot.name}</p>
+              <p className="text-sm font-thin opacity-70">{spot.address.split(",").join(", ")}</p>
+            </div>
+          </Link>
         ))}
-        <div className="flex items-center justify-center">
-          <Button size="lg" isLoading={spotFetcher.state === "loading"} variant="outline" onClick={onNext}>
-            Load more
-          </Button>
-        </div>
+        {count > 10 && (
+          <div className="flex items-center justify-center">
+            <Button size="lg" isLoading={spotFetcher.state === "loading"} variant="outline" onClick={onNext}>
+              Load more
+            </Button>
+          </div>
+        )}
       </div>
     </div>
   )
