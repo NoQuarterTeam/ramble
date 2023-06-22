faef3496a66f129b930a8dfb2099ffd32c8d97c0
Jack Clackett <jack@noquarter.co>
2023-06-22 12:27:35 +0200

    better list detail spots

--- packages/api/src/router/list.ts
+++ packages/api/src/router/list.ts
@@ -1,10 +1,55 @@
 import { z } from "zod"
 import { createTRPCRouter, protectedProcedure, publicProcedure } from "../trpc"
+import { TRPCError } from "@trpc/server"
 
 export const listRouter = createTRPCRouter({
-  detail: publicProcedure.input(z.object({ id: z.string() })).query(({ ctx, input }) => {
+  detail: publicProcedure.input(z.object({ id: z.string() })).query(async ({ ctx, input }) => {
     // TODO: check if user is public
-    return ctx.prisma.list.findUnique({ where: { id: input.id }, include: { listSpots: { include: { spot: true } } } })
+    const list = await ctx.prisma.list.findFirst({
+      where: { id: input.id },
+      select: {
+        id: true,
+        creatorId: true,
+        creator: { select: { username: true, firstName: true, lastName: true } },
+        name: true,
+        description: true,
+        listSpots: {
+          select: {
+            id: true,
+            spot: {
+              select: {
+                type: true,
+                reviews: { select: { rating: true } },
+                id: true,
+                name: true,
+                address: true,
+                description: true,
+                verifier: { select: { username: true, firstName: true, lastName: true, avatar: true } },
+                images: { take: 1 },
+              },
+            },
+          },
+        },
+      },
+    })
+    if (!list) throw new TRPCError({ code: "NOT_FOUND" })
+
+    const formattedList = {
+      ...list,
+      listSpots: list.listSpots.map((listSpot) => ({
+        ...listSpot,
+        spot: {
+          ...listSpot.spot,
+          image: listSpot.spot.images[0]?.path,
+          rating:
+            listSpot.spot.reviews.length > 0
+              ? Math.round(listSpot.spot.reviews.reduce((acc, review) => acc + review.rating, 0) / listSpot.spot.reviews.length)
+              : null,
+        },
+      })),
+    }
+
+    return formattedList
   }),
   savedLists: protectedProcedure.input(z.object({ spotId: z.string() })).query(async ({ ctx, input }) => {
     return ctx.prisma.list.findMany({
