ec56360d5577db9f3f275f8b4aa1c1ed8a620ec5
Jack Clackett <jack@noquarter.co>
2023-07-03 17:41:44 +0200

    remove blur

--- apps/remix/app/components/ui/Modal.tsx
+++ apps/remix/app/components/ui/Modal.tsx
@@ -24,7 +24,7 @@ const ModalOverlay = React.forwardRef<
 >(({ className, children, ...props }, ref) => (
   <ModalPrimitive.Overlay
     className={merge(
-      "data-[state=closed]:animate-out data-[state=open]:fade-in data-[state=closed]:fade-out fixed inset-0 z-50 bg-black/50 backdrop-blur-sm transition-all duration-100",
+      "data-[state=closed]:animate-out data-[state=open]:fade-in data-[state=closed]:fade-out fixed inset-0 z-50 bg-black/50 transition-all duration-100",
       className,
     )}
     {...props}
