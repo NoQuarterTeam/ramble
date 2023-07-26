9dfb855b5d2b7304896d18c517b07fe995f7df7c
Jack Clackett <jack@noquarter.co>
2023-07-26 17:53:22 +0100

    coming soon

--- apps/remix/app/pages/_main+/index.tsx
+++ apps/remix/app/pages/_main+/index.tsx
@@ -10,6 +10,7 @@ import { OptimizedImage } from "~/components/OptimisedImage"
 import { db } from "~/lib/db.server"
 
 import { PageContainer } from "../../components/PageContainer"
+import { Badge } from "~/components/ui"
 
 export const loader = async () => {
   const spots: Array<SpotItemWithImageAndRating> = await db.$queryRaw`
@@ -37,6 +38,7 @@ export default function Home() {
       <PageContainer>
         <div className="grid grid-cols-1 gap-6 py-10 md:grid-cols-9 md:py-32">
           <div className="col-span-9 space-y-2 md:col-span-6">
+            <Badge colorScheme="green">Coming soon</Badge>
             <h1 className="text-4xl font-medium">Everything you need for travelling Europe</h1>
             <p className="text-2xl">1000's of spots, service stations, cafes, parks and more</p>
             <LinkButton size="lg" to="map" className="max-w-min">
