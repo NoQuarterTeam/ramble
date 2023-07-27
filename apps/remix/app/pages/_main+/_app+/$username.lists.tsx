b5a9ef9812f3ed7f698651f606b03ff80508da73
Jack Clackett <jack@noquarter.co>
2023-07-27 15:53:22 +0100

    tweak layout

--- apps/remix/app/pages/_main+/_app+/$username.lists.tsx
+++ apps/remix/app/pages/_main+/_app+/$username.lists.tsx
@@ -1,11 +1,5 @@
 import { Outlet } from "@remix-run/react"
 
-import { PageContainer } from "~/components/PageContainer"
-
-export default function ListDetail() {
-  return (
-    <PageContainer>
-      <Outlet />
-    </PageContainer>
-  )
+export default function UserListsLayout() {
+  return <Outlet />
 }
