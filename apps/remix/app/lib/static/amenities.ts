3174fb5f3cea1cfb35c0f9e1594404679c8cec43
Jack Clackett <jack@noquarter.co>
2023-07-27 11:15:39 +0100

    bbq icon

--- apps/remix/app/lib/static/amenities.ts
+++ apps/remix/app/lib/static/amenities.ts
@@ -1,9 +1,9 @@
-import { Beef, Droplet, Flame, ShowerHead, Thermometer, UtensilsCrossed, Wifi, Zap } from "lucide-react"
+import { Droplet, Flame, ShowerHead, Thermometer, UtensilsCrossed, Wifi, Zap } from "lucide-react"
 
 import { Icons } from "~/components/ui"
 
 export const AMENITIES_ICONS = {
-  bbq: Beef,
+  bbq: Icons.Bbq,
   electricity: Zap,
   water: Droplet,
   toilet: Icons.Toilet,
