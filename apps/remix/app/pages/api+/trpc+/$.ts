9aaff7b6d4d1431ec6ce0e9127b54b5f3526ff42
Jack Clackett <jack@noquarter.co>
2023-08-23 15:48:46 +0200

    add logs to api endpoint

--- apps/remix/app/pages/api+/trpc+/$.ts
+++ apps/remix/app/pages/api+/trpc+/$.ts
@@ -2,6 +2,7 @@ import * as trpcFetch from "@trpc/server/adapters/fetch"
 import type { ActionArgs, LoaderArgs } from "@vercel/remix"
 
 import { appRouter, createContext } from "@ramble/api"
+import { IS_DEV } from "~/lib/config.server"
 
 function handleRequest(args: LoaderArgs | ActionArgs) {
   return trpcFetch.fetchRequestHandler({
@@ -9,6 +10,14 @@ function handleRequest(args: LoaderArgs | ActionArgs) {
     req: args.request,
     router: appRouter,
     createContext,
+    onError: ({ error, path }) => {
+      if (!IS_DEV) {
+        console.error("--- ERROR ---")
+        console.error("Path: " + path)
+        console.error("Message: " + error.message)
+        // TODO: send to sentry
+      }
+    },
   })
 }
 
