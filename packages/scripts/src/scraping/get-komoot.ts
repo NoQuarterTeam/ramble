0b6c8ed4f2f430b6a68b8dea057204f7144a2f79
George Borg <george@noquarter.co>
2023-09-21 12:43:50 +0200

    feat: komoot

--- packages/scripts/src/scraping/get-komoot.ts
+++ packages/scripts/src/scraping/get-komoot.ts
@@ -1,7 +1,10 @@
 import * as cheerio from "cheerio"
 
 // mtb
-const url = `https://www.komoot.com/api/v007/discover_tours/?srid=4326&format=simple&fields=timeline&timeline_highlights_fields=images,start_point&limit=100&max_distance=30000&sport=mtb&page=0&hl=en`
+const url = `https://www.komoot.com/api/v007/discover_tours/?srid=4326&format=simple&fields=timeline&timeline_highlights_fields=images,start_point&limit=100&max_distance=500000&sport=mtb&surface=prefer_unpaved&hl=en`
+
+// hiking
+// const url = `https://www.komoot.com/api/v007/discover_tours/?srid=4326&format=simple&fields=timeline&timeline_highlights_fields=images,start_point&limit=100&max_distance=500000&sport=hike&surface=prefer_unpaved&hl=en`
 
 import exampleData from "./komoot.json"
 
@@ -9,66 +12,83 @@ import { prisma } from "@ramble/database"
 import { SpotType } from "@ramble/database/types"
 
 async function getCards({ lat, lng }: { lat: number; lng: number }) {
-  const res = await fetch(url + `&lat=${lat}&lng=${lng}`)
+  const res = await fetch(url + `&lat=${lat}&lng=${lng}&page=0`)
   const newData = await res.json()
-  const newSpots = newData._embedded.items as any[]
-  console.log(newSpots[0])
+  const totalPages = newData.page.totalPages
+  console.log("Total Pages: " + totalPages)
 
-  const dbSpots = await prisma.spot.findMany({
-    select: { komootId: true },
-    where: { type: "MOUNTAIN_BIKING", komootId: { in: newSpots.map((s) => parseInt(s.id)) } },
-  })
+  for (let page = 0; page < totalPages; page++) {
+    console.log("Page: " + page)
 
-  // number of new spots
+    const res = await fetch(url + `&lat=${lat}&lng=${lng}&page=${page}`)
+    const newData = await res.json()
+    const newSpots = newData?._embedded?.items as any[]
+    console.log("Spots found: " + newSpots.length)
+    const dbSpots = await prisma.spot.findMany({
+      select: { komootId: true },
+      where: { type: "MOUNTAIN_BIKING", komootId: { in: newSpots?.map((s) => s.id) } },
+    })
 
-  for (let index = 0; index < newSpots.length; index++) {
-    const spot = newSpots[index]
+    // number of new spots
 
-    try {
-      // if in db, continue
-      const exists = dbSpots.find((s) => s.komootId === spot.id)
+    for (let index = 0; index < newSpots.length; index++) {
+      const spot = newSpots[index]
+      console.log("Addind spot: " + index + " out of " + newSpots.length)
+      try {
+        // if in db, continue
+        const exists = dbSpots.find((s) => s.komootId === spot.id)
+        console.log(exists ? "Spot already exists" : "...")
 
-      if (exists) continue
-      const data = {
-        type: SpotType.MOUNTAIN_BIKING,
-        komootId: spot.id,
-        address: spot.address.street + ", " + spot.address.city + ", " + spot.address.country + ", " + spot.address.zipcode,
-        name: spot.title_short.replace(/^\(\d+\)\s*/, "").trim(),
-        createdAt: new Date(spot.created_at),
-        description: spot.description,
-        latitude: spot.lat,
-        longitude: spot.lng,
-      }
+        if (exists) continue
+        const data = {
+          type: SpotType.MOUNTAIN_BIKING,
+          // type: SpotType.HIKING,
+          komootId: spot.id,
+          name: spot.name,
+          createdAt: new Date(spot.date),
+          latitude: spot.start_point.lat,
+          longitude: spot.start_point.lng,
+        }
 
-      const detailPage = await fetch(`https://www.komoot.com/smarttour/${spot.id}`)
-      const text = await detailPage.text()
+        const detailPage = await fetch(`https://www.komoot.com/smarttour/${spot.id}`)
+        const text = await detailPage.text()
 
-      const $ = cheerio.load(text)
+        const $ = cheerio.load(text)
 
-      let images: string[] = []
-      $(".css-1ganbmd").each((_, img) => {
-        const src = $(img).attr("data-src")
-        if (src) images.push(src.split("?")[0])
-      })
+        let images: string[] = []
+        $(".css-1ganbmd .css-1sjk0s2").each((_, img) => {
+          const src = $(img).attr("data-src")
+          if (src) images.push(src.split("?")[0])
+        })
+        const uniqueImages = new Set(images)
 
-      await prisma.spot.create({
-        data: {
-          ...data,
-          images: {
-            create: images.map((url) => ({ path: url, creator: { connect: { email: "george@noquarter.co" } } })),
+        await prisma.spot.create({
+          data: {
+            ...data,
+            images: {
+              create:
+                uniqueImages &&
+                Array.from(uniqueImages).map((url) => ({ path: url, creator: { connect: { email: "george@noquarter.co" } } })),
+            },
+            creator: { connect: { email: "george@noquarter.co" } },
           },
-          creator: { connect: { email: "george@noquarter.co" } },
-        },
-      })
-    } catch {}
+        })
+      } catch (error) {
+        console.log(error)
+      }
+    }
   }
 }
 
 async function main() {
   try {
-    // lat and lng coords of europe in large squares
-    // 52.3759° N, 4.8975° E
-    await getCards({ lat: 52.3759, lng: 4.8975 })
+    for (let lat = 40; lat < 75; lat = lat + 10) {
+      console.log("Lat: " + lat)
+      for (let lng = 5; lng < 30; lng = lng + 10) {
+        console.log("Lng: " + lng)
+        await getCards({ lat, lng })
+      }
+    }
   } catch (error) {
     console.log(error)
     process.exit(1)
