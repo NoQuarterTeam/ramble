68043e840095e96b4f570510a13a8e38fb1cc0f3
Jack Clackett <jack@noquarter.co>
2023-10-09 12:39:18 +0200

    email layout again

--- apps/remix/app/pages/admin+/emails+/index.tsx
+++ apps/remix/app/pages/admin+/emails+/index.tsx
@@ -2,7 +2,7 @@ import { Mail } from "lucide-react"
 
 export default function Index() {
   return (
-    <div className="center h-screen w-full">
+    <div className="center pt-10">
       <div className="vstack rounded-xs border p-8">
         <div>
           <Mail size={30} strokeWidth={1} />
