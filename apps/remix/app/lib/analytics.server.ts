9f1cceaecb0833b89bc8f8c3364e90f6cdb74410
Jack Clackett <jack@noquarter.co>
2023-10-10 16:20:52 +0200

    patch anaytics package

--- apps/remix/app/lib/analytics.server.ts
+++ apps/remix/app/lib/analytics.server.ts
@@ -1,6 +1,6 @@
 "use server"
-// export { track } from "@vercel/analytics/server"
+export { track } from "@vercel/analytics/server"
 
-export function track(name: string, data: Record<string, unknown>) {
-  // todo
-}
+// export function track(name: string, data: Record<string, unknown>) {
+//   // todo
+// }
