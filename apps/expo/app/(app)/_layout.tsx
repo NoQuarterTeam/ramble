ae80ada2ea2c5c89fbeeee18b731dbd5827da4d9
Jack Clackett <jack@noquarter.co>
2023-10-12 21:58:47 +0200

    tweaks to darkmode

--- apps/expo/app/(app)/_layout.tsx
+++ apps/expo/app/(app)/_layout.tsx
@@ -23,9 +23,9 @@ export function AppLayout() {
   return (
     <Tab.Navigator
       initialRouteName="MapLayout"
-      sceneContainerStyle={{ backgroundColor: backgroundColor }}
+      sceneContainerStyle={{ backgroundColor }}
       screenOptions={{
-        tabBarStyle: { backgroundColor: backgroundColor },
+        tabBarStyle: { backgroundColor, borderTopColor: colors.gray[isDark ? 700 : 50] },
         headerShown: false,
         tabBarShowLabel: false,
       }}
