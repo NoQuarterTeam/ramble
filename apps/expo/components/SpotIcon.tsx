6f4e613fa4f229d6679dd649072753f1f86fc727
Jack Clackett <jack@noquarter.co>
2023-10-26 12:05:12 +0200

    fix spot icons

--- apps/expo/components/SpotIcon.tsx
+++ apps/expo/components/SpotIcon.tsx
@@ -3,12 +3,19 @@ import { type LucideProps } from "lucide-react-native"
 import { type SpotType } from "@ramble/database/types"
 
 import { SPOT_TYPES } from "../lib/models/spot"
+import { Icon, IconColors } from "./Icon"
 
 interface Props extends LucideProps {
   type: SpotType
+  color?: IconColors
 }
 
 export function SpotIcon({ type, ...props }: Props) {
+  const Comp = SPOT_TYPES[type].Icon
+  return <Icon icon={Comp} {...props} />
+}
+
+export function SpotIconMap({ type, ...props }: Props) {
   const Comp = SPOT_TYPES[type].Icon
   return <Comp {...props} />
 }
