ced7af8a59d1b8b20057558f9302e9ee83649d6a
Jack Clackett <jack@noquarter.co>
2023-11-02 10:42:18 +0100

    tweak slack message

--- apps/remix/app/lib/slack.server.ts
+++ apps/remix/app/lib/slack.server.ts
@@ -1,11 +1,14 @@
 import { WebClient } from "@slack/web-api"
 import { SLACK_TOKEN } from "./env.server"
+import { IS_PRODUCTION } from "./config.server"
 
 export const slack = new WebClient(SLACK_TOKEN)
 
+const username = IS_PRODUCTION ? "Ramble bot" : "Ramble bot (dev)"
+
 export function sendSlackMessage(text: string) {
   try {
-    void slack.chat.postMessage({ channel: "C05TPL2FS9X", text })
+    void slack.chat.postMessage({ username, channel: "C05TPL2FS9X", text, icon_url: "https://ramble.guide/icon-dark.png" })
   } catch (error) {
     console.log(error)
   }
