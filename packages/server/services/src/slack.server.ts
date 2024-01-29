6a56f86b13236a373ad0771a9302c0ef0f336deb
Jack Clackett <jack@noquarter.co>
2024-01-29 12:38:25 +0100

    add new spot notifier

--- packages/server/services/src/slack.server.ts
+++ packages/server/services/src/slack.server.ts
@@ -1,4 +1,4 @@
-import { WebClient } from "@slack/web-api"
+import { type KnownBlock, WebClient } from "@slack/web-api"
 
 import { env, IS_PRODUCTION } from "@ramble/server-env"
 
@@ -6,10 +6,16 @@ export const slack = new WebClient(env.SLACK_TOKEN)
 
 const username = IS_PRODUCTION ? "Ramble bot" : "Ramble bot (dev)"
 
-export async function sendSlackMessage(text: string) {
+export async function sendSlackMessage(text: string, blocks?: KnownBlock[]) {
   try {
     if (!IS_PRODUCTION) return console.log("Slack disabled in dev mode, message: ", text)
-    await slack.chat.postMessage({ username, channel: "C05TPL2FS9X", text, icon_url: "https://ramble.guide/logo-dark.png" })
+    await slack.chat.postMessage({
+      username,
+      channel: "C05TPL2FS9X",
+      text,
+      blocks,
+      icon_url: "https://ramble.guide/logo-dark.png",
+    })
   } catch (error) {
     console.log(error)
   }
