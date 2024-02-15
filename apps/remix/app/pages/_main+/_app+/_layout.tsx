3fed9f0c627a0f4ce7aed377c9e6865b4ad0ec26
Jack Clackett <jack@noquarter.co>
2024-02-15 19:25:18 +0100

    fix redirect again

--- apps/remix/app/pages/_main+/_app+/_layout.tsx
+++ apps/remix/app/pages/_main+/_app+/_layout.tsx
@@ -1,21 +1,19 @@
-import { Outlet, ShouldRevalidateFunction, redirect } from "@remix-run/react"
+import { Outlet, ShouldRevalidateFunction } from "@remix-run/react"
 
 import { json } from "~/lib/remix.server"
 import { type SerializeFrom, type LoaderFunctionArgs } from "~/lib/vendor/vercel.server"
 import { type MapLayers, defaultMapLayers, mapLayersCookies } from "~/pages/api+/map-layers"
-import { getUserSession } from "~/services/session/session.server"
+
 import { mapLayersUrl } from "./components/MapLayerControls"
+import { requireUser } from "~/services/auth/auth.server"
 
 export const shouldRevalidate: ShouldRevalidateFunction = ({ formAction }) => {
   return formAction === mapLayersUrl
 }
 
 export const loader = async ({ request }: LoaderFunctionArgs) => {
-  const { userId } = await getUserSession(request)
-  const url = new URL(request.url)
+  await requireUser(request)
   const cookieHeader = request.headers.get("Cookie")
-
-  if (!userId && url.pathname !== "/") throw redirect("/")
   const mapLayers: MapLayers = await mapLayersCookies.parse(cookieHeader)
   return json({ mapLayers: mapLayers || defaultMapLayers })
 }
