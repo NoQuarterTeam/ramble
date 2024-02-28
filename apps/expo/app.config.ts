8f0eef670cf813bb26e2d72d52fc94d1b1010eae
Jack Clackett <jack@noquarter.co>
2024-02-28 13:49:56 +0100

    b48

--- apps/expo/app.config.ts
+++ apps/expo/app.config.ts
@@ -1,7 +1,7 @@
 import { ConfigContext, ExpoConfig } from "expo/config"
 
 const VERSION = "1.3.1"
-const BUILD = 47
+const BUILD = 48
 
 const splash: ExpoConfig["splash"] = {
   image: "./assets/splash.png",
