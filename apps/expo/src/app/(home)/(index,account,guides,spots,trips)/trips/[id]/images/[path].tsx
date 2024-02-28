d043174d4ae7e0c7a9c5d21b251e11dc125b0b6b
Jack Clackett <jack@noquarter.co>
2024-02-28 18:07:56 +0100

    image screens

--- apps/expo/src/app/(home)/(index,account,guides,spots,trips)/trips/[id]/images/[path].tsx
+++ apps/expo/src/app/(home)/(index,account,guides,spots,trips)/trips/[id]/images/[path].tsx
@@ -1,14 +1,17 @@
 import { createImageUrl } from "@ramble/shared"
 import { Image } from "expo-image"
 import { useLocalSearchParams } from "expo-router"
+import { View } from "react-native"
 import { ScreenView } from "~/components/ui/ScreenView"
 
 export default function TripImage() {
   const { path } = useLocalSearchParams<{ path: string }>()
 
   return (
-    <ScreenView title="Image">
-      <Image source={{ uri: createImageUrl(path) }} style={{ width: "100%", height: 400 }} />
+    <ScreenView title="" containerClassName="px-0">
+      <View className="pb-2 flex-1">
+        <Image source={{ uri: createImageUrl(path) }} className="flex-1 h-full" contentFit="contain" />
+      </View>
     </ScreenView>
   )
 }
--- apps/expo/src/app/(home)/(index,account,guides,spots,trips)/trips/[id]/images/index.tsx
+++ apps/expo/src/app/(home)/(index,account,guides,spots,trips)/trips/[id]/images/index.tsx
@@ -1,18 +1,29 @@
 import { createImageUrl } from "@ramble/shared"
 import { Image } from "expo-image"
-import { useLocalSearchParams } from "expo-router"
+import { Link, useLocalSearchParams } from "expo-router"
+import { ScrollView, TouchableOpacity, View } from "react-native"
 import { ScreenView } from "~/components/ui/ScreenView"
+import { width } from "~/lib/device"
 
+const size = (width - 16) / 3
 export default function TripImages() {
-  const { images } = useLocalSearchParams<{ images?: string }>()
+  const { id, images } = useLocalSearchParams<{ id: string; images?: string }>()
 
   const imageUrls = images?.split(",")
 
   return (
-    <ScreenView title="Images">
-      {imageUrls?.map((url) => (
-        <Image key={url} source={{ uri: createImageUrl(url) }} style={{ width: 100, height: 100 }} />
-      ))}
+    <ScreenView title="" containerClassName="px-0">
+      <ScrollView className="flex-1 pt-2">
+        <View className="flex flex-wrap flex-row gap-2">
+          {imageUrls?.map((url) => (
+            <Link key={url} href={`/(home)/(trips)/trips/${id}/images/${url}`} asChild>
+              <TouchableOpacity>
+                <Image source={{ uri: createImageUrl(url) }} style={{ width: size, height: size }} />
+              </TouchableOpacity>
+            </Link>
+          ))}
+        </View>
+      </ScrollView>
     </ScreenView>
   )
 }
