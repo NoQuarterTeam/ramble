8de51004123855d61b9001f58a626cf71182d577
Jack Clackett <jack@noquarter.co>
2024-03-25 17:13:43 +0100

    fix

--- packages/api/src/router/s3.ts
+++ packages/api/src/router/s3.ts
@@ -10,9 +10,8 @@ export const s3Router = createTRPCRouter({
   /**
    * @deprecated use createSignedUrlNew
    */
-  createSignedUrl: protectedProcedure.input(z.object({ key: z.string().min(1) })).mutation(() => {
-    const key = randomUUID()
-    return createSignedUrl(key)
+  createSignedUrl: protectedProcedure.input(z.object({ key: z.string().min(1) })).mutation(({ input }) => {
+    return createSignedUrl(input.key)
   }),
   createSignedUrlNew: protectedProcedure.input(z.object({ type: z.string() })).mutation(async ({ input }) => {
     const uuid = randomUUID()
