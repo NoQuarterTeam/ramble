31eeaa5282a06f3f524b310adb8166f14c2bfd10
Jack Clackett <jack@noquarter.co>
2024-04-03 15:39:17 +0200

    update deps, v.1.4.5

--- apps/expo/app.config.ts
+++ apps/expo/app.config.ts
@@ -1,7 +1,7 @@
 import type { ConfigContext, ExpoConfig } from "expo/config"
 
-const VERSION = "1.4.4"
-const BUILD = 58
+const VERSION = "1.4.5"
+const BUILD = 59
 
 const splash: ExpoConfig["splash"] = {
   image: "./assets/splash.png",
