414728523aff2186fe4740abdf44142c584a8f6c
Jack Clackett <jack@noquarter.co>
2024-04-23 19:46:51 +0200

    posthog

--- apps/next/src/app/layout.tsx
+++ apps/next/src/app/layout.tsx
@@ -1,10 +1,15 @@
-import type { Metadata } from "next"
-import { Urbanist } from "next/font/google"
-
 import { Nav } from "@/components/Nav"
+import { PosthogProvider } from "@/components/PosthogProvider"
 import { TRPCReactProvider } from "@/lib/trpc/react"
 import "mapbox-gl/dist/mapbox-gl.css"
+import type { Metadata } from "next"
+import dynamic from "next/dynamic"
+import { Urbanist } from "next/font/google"
 import "./globals.css"
+
+const PostHogPageView = dynamic(() => import("../components/PosthogPageView"), {
+  ssr: false,
+})
 
 const font = Urbanist({ subsets: ["latin"], variable: "--font-urbanist" })
 
@@ -16,12 +21,15 @@ export const metadata: Metadata = {
 export default function RootLayout({ children }: Readonly<{ children: React.ReactNode }>) {
   return (
     <html lang="en" className="dark">
-      <TRPCReactProvider>
-        <body className={font.className}>
-          <Nav />
-          <div className="pt-nav">{children}</div>
-        </body>
-      </TRPCReactProvider>
+      <PosthogProvider>
+        <TRPCReactProvider>
+          <body className={font.className}>
+            <Nav />
+            <PostHogPageView />
+            <div className="pt-nav">{children}</div>
+          </body>
+        </TRPCReactProvider>
+      </PosthogProvider>
     </html>
   )
 }
