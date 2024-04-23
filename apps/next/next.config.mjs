b9ca20a392f27a76012c02fd9f570ef99af7e1d1
Jack Clackett <jack@noquarter.co>
2024-04-23 18:27:58 +0200

    add missing source urls to next

--- apps/next/next.config.mjs
+++ apps/next/next.config.mjs
@@ -21,6 +21,12 @@ const nextConfig = {
       { hostname: "hipcamp-res.cloudinary.com" },
       { hostname: "polskicaravaning.pl" },
       { hostname: "spots.roadsurfer.com" },
+      { hostname: "www.norcamp.de" },
+      { hostname: "cucortu.ro" },
+      { hostname: "image.thecrag.com" },
+      { hostname: "cdn3.park4night.com" },
+      { hostname: "d2exd72xrrp1s7.cloudfront.net" },
+      { hostname: "lh3.googleusercontent.com" },
     ],
   },
 }
