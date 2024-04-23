d9bcdd96b31cf4cce6a3d0c223b2b6ab4ab30441
Jack Clackett <jack@noquarter.co>
2024-04-23 18:34:06 +0200

    ppr?

--- apps/next/next.config.mjs
+++ apps/next/next.config.mjs
@@ -2,6 +2,7 @@
 const nextConfig = {
   eslint: { ignoreDuringBuilds: true },
   typescript: { ignoreBuildErrors: true },
+  experimental: { ppr: true },
   transpilePackages: [
     "axios",
     "decode-uri-component",
