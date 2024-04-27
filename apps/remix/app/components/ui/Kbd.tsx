99c6b3ddca91103623cc0ba8cef5c5d2f36b118c
Jack Clackett <jack@noquarter.co>
2024-04-27 21:38:02 +0200

    remove remix

--- apps/remix/app/components/ui/Kbd.tsx
+++
@@ -1,9 +0,0 @@
-import type * as React from "react"
-
-export function Kbd(props: { children: React.ReactNode }) {
-  return (
-    <span className="whitesspanace-nowrap inline-block rounded-xs border border-black/20 border-b-4 bg-transparent px-1 font-medium font-mono text-xs dark:border-white/20">
-      {props.children}
-    </span>
-  )
-}
--- apps/remix/app/pages/_admin+/_main+/_layout.tsx
+++
@@ -1,9 +0,0 @@
-import { Outlet } from "@remix-run/react"
-
-export default function AdminMainLayout() {
-  return (
-    <div className="px-4 py-8">
-      <Outlet />
-    </div>
-  )
-}
