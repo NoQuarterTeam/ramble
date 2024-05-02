5f36ff16693700fcb23968933123c9fde5f24173
Jack Clackett <jack@noquarter.co>
2024-05-02 13:55:29 +0200

    add preview blogs for admins

--- apps/next/src/app/blog/[slug]/page.tsx
+++ apps/next/src/app/blog/[slug]/page.tsx
@@ -1,16 +1,16 @@
-import type { PageObjectResponse } from "@notionhq/client/build/src/api-endpoints"
-import advancedFormat from "dayjs/plugin/advancedFormat"
-
 import { AppCta } from "@/components/AppCta"
 import { LinkButton } from "@/components/LinkButton"
 import { BLOG_DB_ID, notion } from "@/lib/server/notion"
+import type { PageObjectResponse } from "@notionhq/client/build/src/api-endpoints"
 import dayjs from "dayjs"
+import advancedFormat from "dayjs/plugin/advancedFormat"
 import { ArrowLeft } from "lucide-react"
 import type { Metadata } from "next"
 import Image from "next/image"
 import { NotionBlock } from "../components/NotionBlock"
 import { Tag } from "../components/Tag"
-import { getPageContent } from "./getPageContent"
+import { BlogDetail } from "./BlogDetail"
+import { getBlogContent } from "./getBlogData"
 dayjs.extend(advancedFormat)
 
 export async function generateStaticParams() {
@@ -35,8 +35,7 @@ export async function generateStaticParams() {
 }
 
 export const generateMetadata = async ({ params: { slug } }: { params: { slug: string } }): Promise<Metadata> => {
-  const { title, summary, cover } = await getPageContent(slug)
-
+  const { title, summary, cover } = await getBlogContent(slug)
   return {
     title,
     description: summary,
@@ -50,56 +49,6 @@ export const generateMetadata = async ({ params: { slug } }: { params: { slug: s
 }
 
 export default async function Page({ params }: { params: { slug: string } }) {
-  const slug = params.slug
-  const page = await getPageContent(slug)
-
-  return (
-    <div className="px-4 py-8">
-      <div className="mx-auto max-w-2xl space-y-6">
-        <LinkButton size="sm" leftIcon={<ArrowLeft size={14} />} aria-label="Back to home" variant="outline" href="/blog">
-          Back
-        </LinkButton>
-
-        <div className="flex flex-wrap gap-4 items-center">
-          <div className="flex flex-wrap gap-2">
-            {page.tags.map((tag) => (
-              <Tag key={tag.id} tag={tag} />
-            ))}
-          </div>
-          <p className="opacity-60">{dayjs(page.publishedAt).format("Do MMMM YYYY")}</p>
-        </div>
-        <h1 className="text-4xl font-bold">{page.title}</h1>
-        {page.cover && (
-          <div className="relative">
-            <Image
-              src={page.cover}
-              unoptimized={!page.cover.startsWith("https://cdn.ramble")}
-              alt={page.title}
-              width={800}
-              height={350}
-              className="w-full h-[350px] object-cover rounded-sm"
-            />
-            {page.coverSource && page.coverSourceUrl && (
-              <a
-                href={page.coverSourceUrl}
-                target="_blank"
-                rel="noopener noreferrer"
-                className="absolute bottom-1 text-xs right-1 bg-background px-2 py-1 rounded-sm hover:opacity-75"
-              >
-                Photo by {page.coverSource}
-              </a>
-            )}
-          </div>
-        )}
-        <div>
-          {page.content.map((block) => (
-            <NotionBlock key={block.id} block={block} />
-          ))}
-        </div>
-        <div className="py-20">
-          <AppCta message="Download the app now and explore!" />
-        </div>
-      </div>
-    </div>
-  )
+  const page = await getBlogContent(params.slug)
+  return <BlogDetail page={page} />
 }
