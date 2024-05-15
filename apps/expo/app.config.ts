e823b1bb12dd09f5ab5295c01f86d07ca6a5483e
Jack Clackett <jack@noquarter.co>
2024-05-15 11:48:57 +0200

    temp remove notifications from app

--- apps/expo/app.config.ts
+++ apps/expo/app.config.ts
@@ -1,7 +1,7 @@
 import type { ConfigContext, ExpoConfig } from "expo/config"
 
 const VERSION = "1.5.0"
-const BUILD = 76
+const BUILD = 77
 
 const splash: ExpoConfig["splash"] = {
   image: "./assets/splash.png",
