04249832b8ee83e8a6ecd5c3780d78acc93c3aa9
Daniel Le Cornu <dan@noquarter.co>
2024-05-15 11:17:59 +0200

    add filters number badge to map screen, remove duplicate buttons

--- apps/expo/src/app/(home)/(index,account,guides,spots,trips)/(map)/index.tsx
+++ apps/expo/src/app/(home)/(index,account,guides,spots,trips)/(map)/index.tsx
@@ -331,9 +331,14 @@ function MapContainer() {
           <TouchableOpacity
             onPress={() => increment()}
             activeOpacity={0.8}
-            className="sq-12 shadow flex flex-row items-center justify-center rounded-full bg-background dark:bg-background-dark"
+            className="sq-12 shadow flex flex-row items-center justify-center rounded-full bg-background dark:bg-background-dark relative"
           >
             <Icon icon={Settings2} size={20} />
+            {filters.types.length > 0 && (
+              <View className="absolute top-[-6px] right-[-6px] bg-orange-500 rounded-full w-[24px] h-[24px]">
+                <Text className="text-white text-sm text-center font-700 leading-6">{filters.types.length}</Text>
+              </View>
+            )}
           </TouchableOpacity>
         </Link>
       </View>
@@ -343,38 +348,6 @@ function MapContainer() {
       >
         <Icon icon={Navigation} size={20} />
       </TouchableOpacity>
-      <TouchableOpacity
-        activeOpacity={0.8}
-        onPress={() => {
-          increment()
-          router.push("/new/")
-        }}
-        style={{ transform: [{ translateX: -26 }] }}
-        className="absolute bottom-3 left-1/2 shadow rounded-full bg-primary p-4"
-      >
-        <Icon icon={PlusCircle} size={20} color="white" />
-      </TouchableOpacity>
-
-      <View pointerEvents="box-none" className="absolute bottom-3 left-3 flex space-y-2">
-        <Link push href={"/layers"} asChild>
-          <TouchableOpacity
-            onPress={() => increment()}
-            activeOpacity={0.8}
-            className="sq-12 flex flex-row items-center shadow justify-center rounded-full bg-background dark:bg-background-dark"
-          >
-            <Icon icon={Layers} size={20} />
-          </TouchableOpacity>
-        </Link>
-        <Link push href={"/filters"} asChild>
-          <TouchableOpacity
-            onPress={() => increment()}
-            activeOpacity={0.8}
-            className="sq-12 flex flex-row items-center shadow justify-center rounded-full bg-background dark:bg-background-dark"
-          >
-            <Icon icon={Settings2} size={20} />
-          </TouchableOpacity>
-        </Link>
-      </View>
       {activeSpotId && <SpotPreview id={activeSpotId} onSetSpotId={setActiveSpotId} />}
       {selectedBioRegion && <BioRegionPreview id={selectedBioRegion} onClose={handleCloseBioRegionPreview} />}
     </>
