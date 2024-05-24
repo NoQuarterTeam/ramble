d985c947cca16a38b90ecb4c45d7827df6588d7c
Jack Clackett <jack@noquarter.co>
2024-05-24 17:13:41 +0200

    test fixing blog

--- apps/next/src/app/blog/page.tsx
+++ apps/next/src/app/blog/page.tsx
@@ -2,12 +2,12 @@ import dayjs from "dayjs"
 import advancedFormat from "dayjs/plugin/advancedFormat"
 import Image from "next/image"
 import Link from "next/link"
+import { Suspense } from "react"
 import { Tag } from "./components/Tag"
 import { getBlogPosts } from "./getBlogPosts"
 dayjs.extend(advancedFormat)
 
-export default async function Page() {
-  const items = await getBlogPosts()
+export default function Page() {
   return (
     <div className="px-4 py-8">
       <div className="mx-auto max-w-6xl  space-y-6">
@@ -18,36 +18,44 @@ export default async function Page() {
             doesn't seem to have any clear organization or purpose
           </p>
         </div>
-
-        <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
-          {items.map((item) => (
-            <Link
-              key={item.id}
-              href={`/blog/${item.slug.toLocaleLowerCase()}`}
-              className="p-4 space-y-2 border rounded-sm hover:border-gray-600 duration-200 cursor-pointer"
-            >
-              {item.cover && (
-                <Image
-                  src={item.cover}
-                  unoptimized={!item.cover.startsWith("https://cdn.ramble")}
-                  alt={item.title}
-                  width={600}
-                  height={300}
-                  className="rounded-sm h-[300px] w-full object-cover"
-                />
-              )}
-              <p className="opacity-60">{dayjs(item.publishedAt).format("Do MMMM YYYY")}</p>
-              <p className="text-2xl font-semibold leading-8">{item.title}</p>
-              <p className="line-clamp-3 font-light">{item.summary}</p>
-              <div className="flex flex-wrap gap-2">
-                {item.tags.map((tag) => (
-                  <Tag key={tag.id} tag={tag} />
-                ))}
-              </div>
-            </Link>
-          ))}
-        </div>
+        <Suspense>
+          <Items />
+        </Suspense>
       </div>
+    </div>
+  )
+}
+
+async function Items() {
+  const items = await getBlogPosts()
+  return (
+    <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
+      {items.map((item) => (
+        <Link
+          key={item.id}
+          href={`/blog/${item.slug.toLocaleLowerCase()}`}
+          className="p-4 space-y-2 border rounded-sm hover:border-gray-600 duration-200 cursor-pointer"
+        >
+          {item.cover && (
+            <Image
+              src={item.cover}
+              unoptimized={!item.cover.startsWith("https://cdn.ramble")}
+              alt={item.title}
+              width={600}
+              height={300}
+              className="rounded-sm h-[300px] w-full object-cover"
+            />
+          )}
+          <p className="opacity-60">{dayjs(item.publishedAt).format("Do MMMM YYYY")}</p>
+          <p className="text-2xl font-semibold leading-8">{item.title}</p>
+          <p className="line-clamp-3 font-light">{item.summary}</p>
+          <div className="flex flex-wrap gap-2">
+            {item.tags.map((tag) => (
+              <Tag key={tag.id} tag={tag} />
+            ))}
+          </div>
+        </Link>
+      ))}
     </div>
   )
 }
