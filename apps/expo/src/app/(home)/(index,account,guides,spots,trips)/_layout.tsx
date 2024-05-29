e132321a3257062e61f4f85c803f53a3e969385e
Jack Clackett <jack@noquarter.co>
2024-05-29 17:23:34 +0200

    fix noti images

--- apps/expo/src/app/(home)/(index,account,guides,spots,trips)/_layout.tsx
+++ apps/expo/src/app/(home)/(index,account,guides,spots,trips)/_layout.tsx
@@ -2,6 +2,10 @@ import { Stack } from "expo-router"
 
 import { useBackgroundColor } from "~/lib/tailwind"
 
+export const unstable_settings = {
+  initialRouteName: "(map)",
+}
+
 export default function Layout() {
   const backgroundColor = useBackgroundColor()
   return <Stack screenOptions={{ contentStyle: { backgroundColor }, headerShown: false }} />
