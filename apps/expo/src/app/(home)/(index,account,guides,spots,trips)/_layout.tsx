544130d28aeb9b57973346abb7c552efd1fa3cf6
Jack Clackett <jack@noquarter.co>
2024-05-29 17:47:51 +0200

    revert change

--- apps/expo/src/app/(home)/(index,account,guides,spots,trips)/_layout.tsx
+++ apps/expo/src/app/(home)/(index,account,guides,spots,trips)/_layout.tsx
@@ -2,10 +2,6 @@ import { Stack } from "expo-router"
 
 import { useBackgroundColor } from "~/lib/tailwind"
 
-export const unstable_settings = {
-  initialRouteName: "(map)",
-}
-
 export default function Layout() {
   const backgroundColor = useBackgroundColor()
   return <Stack screenOptions={{ contentStyle: { backgroundColor }, headerShown: false }} />
