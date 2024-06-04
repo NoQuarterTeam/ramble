d85ec49a5cad8f5d7f5fb22b16f326c1064ea1e9
Jack Clackett <jack@noquarter.co>
2024-06-04 22:00:28 +0200

    add nanoids

--- packages/database/index.ts
+++ packages/database/index.ts
@@ -2,6 +2,7 @@ import { Client } from "@planetscale/database"
 import { PrismaPlanetScale } from "@prisma/adapter-planetscale"
 import { PrismaClient } from "@prisma/client"
 import { env } from "@ramble/server-env"
+import { customAlphabet } from "nanoid"
 
 const client = new Client({ url: env.DATABASE_URL })
 
@@ -11,6 +12,16 @@ const globalForPrisma = globalThis as unknown as { prisma: PrismaClient }
 
 // export const prisma = globalForPrisma.prisma || new PrismaClient({ adapter, log: ["query"] })
 export const prisma =
-  globalForPrisma.prisma || new PrismaClient(!env.DATABASE_URL.includes("127.0.0.1") ? { adapter } : undefined)
+  globalForPrisma.prisma ||
+  new PrismaClient(!env.DATABASE_URL.includes("127.0.0.1") ? { adapter } : undefined).$extends({
+    query: {
+      spot: {
+        create: async ({ query, args }) => {
+          const id = customAlphabet("abcdefghjkmnpqrstuvwxyz2345678ABCDEFGHJKLMPQRSTUVXYZ")(8)
+          return query({ ...args, data: { ...args.data, nanoid: id } })
+        },
+      },
+    },
+  })
 
 if (env.NODE_ENV !== "production") globalForPrisma.prisma = prisma
