50f546101046868526ca4ef806522f5a6c5861d8
Daniel Le Cornu <dan@noquarter.co>
2024-06-11 16:46:53 +0200

    remove test code

--- apps/expo/src/lib/fileSystem.ts
+++ apps/expo/src/lib/fileSystem.ts
@@ -1,5 +1,4 @@
 import type * as FileSystem from "expo-file-system"
 
-// export const TEN_MB = 10 * 1024 * 1024
-export const TEN_MB = 1 * 1024 * 1024 // TESTINGGGGG
+export const TEN_MB = 10 * 1024 * 1024
 export type FileInfo = FileSystem.FileInfo & { size?: number }
