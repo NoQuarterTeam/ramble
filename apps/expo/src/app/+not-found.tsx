3d041ac61ac17f7506e0e4873d91bd71aca49963
Jack Clackett <jack@noquarter.co>
2024-06-15 10:02:16 +0200

    fix padding

--- apps/expo/src/app/+not-found.tsx
+++ apps/expo/src/app/+not-found.tsx
@@ -1,14 +1,17 @@
 import { useRouter } from "expo-router"
 import { View } from "react-native"
+import { SafeAreaView } from "~/components/SafeAreaView"
 import { Button } from "~/components/ui/Button"
 import { Text } from "~/components/ui/Text"
 
 export default function NotFound() {
   const router = useRouter()
   return (
-    <View className="space-y-2">
-      <Text>Not Found</Text>
-      <Button onPress={() => router.navigate("/")}>Take me back</Button>
-    </View>
+    <SafeAreaView>
+      <View className="space-y-2 flex p-4">
+        <Text>Not Found</Text>
+        <Button onPress={() => router.navigate("/")}>Take me back</Button>
+      </View>
+    </SafeAreaView>
   )
 }
