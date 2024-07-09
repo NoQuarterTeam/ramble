81c08c10c2f4741b5f30b2c2663be3672293a668
Jack Clackett <jack@noquarter.co>
2024-07-09 14:12:26 +0200

    user cluster rounded

--- apps/expo/src/app/(home)/(index,account,users,spots,trips)/(map)/index.tsx
+++ apps/expo/src/app/(home)/(index,account,users,spots,trips)/(map)/index.tsx
@@ -203,7 +203,7 @@ function MapContainer() {
                 activeOpacity={0.7}
                 onPress={onPress}
                 className={join(
-                  "flex items-center justify-center rounded-sm border border-primary-100 bg-primary-700",
+                  "flex items-center justify-center rounded-full border border-primary-100 bg-primary-700",
                   point.properties.point_count > 150
                     ? "sq-20"
                     : point.properties.point_count > 75
