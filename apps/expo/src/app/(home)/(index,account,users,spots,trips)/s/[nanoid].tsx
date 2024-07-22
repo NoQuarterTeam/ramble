688db3d9b0d1a9a7e26647499a43723fb8a0d1fa
Jack Clackett <jack@noquarter.co>
2024-07-22 16:29:14 +0200

    add spinner

--- apps/expo/src/app/(home)/(index,account,users,spots,trips)/s/[nanoid].tsx
+++ apps/expo/src/app/(home)/(index,account,users,spots,trips)/s/[nanoid].tsx
@@ -1,4 +1,6 @@
 import { Redirect, useLocalSearchParams } from "expo-router"
+import { View } from "react-native"
+import { Spinner } from "~/components/ui/Spinner"
 import { api } from "~/lib/api"
 import { useTabSegment } from "~/lib/hooks/useTabSegment"
 
@@ -6,7 +8,12 @@ export default function Page() {
   const params = useLocalSearchParams<{ nanoid: string }>()
   const { data, isLoading } = api.spot.byNanoid.useQuery({ nanoid: params.nanoid || "" }, { staleTime: Number.POSITIVE_INFINITY })
   const tab = useTabSegment()
-  if (isLoading) return null
+  if (isLoading)
+    return (
+      <View className="pt-20 flex items-center justify-center w-full">
+        <Spinner />
+      </View>
+    )
   if (data?.id) <Redirect href={`/${tab}/spot/${data.id}`} />
-  else <Redirect href={`/${tab}`} />
+  else <Redirect href={`/${tab}/(map)`} />
 }
