b8de425cdeccb07d482a17bd55b3409518d66cf0
Jack Clackett <jack@noquarter.co>
2024-11-01 19:12:54 +0100

    fix

--- packages/database/index.ts
+++ packages/database/index.ts
@@ -1,19 +1,12 @@
-import { Client } from "@planetscale/database"
-import { PrismaPlanetScale } from "@prisma/adapter-planetscale"
 import { PrismaClient } from "@prisma/client"
 import { env } from "@ramble/server-env"
 import { createSpotNanoId } from "./nanoid"
 
-const client = new Client({ url: env.DATABASE_URL })
-
-const adapter = new PrismaPlanetScale(client)
-
 const globalForPrisma = globalThis as unknown as { prisma: PrismaClient }
 
-// export const prisma = globalForPrisma.prisma || new PrismaClient({ adapter, log: ["query"] })
 export const prisma =
   globalForPrisma.prisma ||
-  new PrismaClient(!env.DATABASE_URL.includes("127.0.0.1") ? { adapter } : undefined).$extends({
+  new PrismaClient().$extends({
     query: {
       spot: {
         create: ({ query, args }) => {
